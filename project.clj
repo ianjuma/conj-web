b905d09fa7132977abb84e5983be8f29f7af9df5
ianjuma <ian.juma@yahoo.com>
2014-02-10 20:02:57 +0300

    project def + deps

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject conj-web "0.1.0-SNAPSHOT"
+  :description "shouter"
+  :url "https://github.com/ianjuma/conj-web"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [ring/ring-jetty-adapter "1.2.1"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]]
+  :main shouter.core
+  :aot [shouter.core])
